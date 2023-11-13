1cfc3f520307f841e2afc1f66552ac5994b2f0d2
Camilla Centerwall <camilla.centerwall@iths.se>
2023-11-13 13:19:38 +0100

    Added portfolio

---
+++ script.js
@@ -0,0 +1,74 @@
+// var firstHeading = document.getElementsByClassName("hello")[0];
+// var myButton = document.getElementById("button");
+// var myText = "Hello World x";
+// var myNumber = 2;
+// function changeTheText() {
+//   firstHeading.innerHTML = myText + myNumber;
+// }
+// myButton.addEventListener("click", changeTheText);
+
+// var ux = ["user research", "IA", "IxD", "UI"];
+
+// console.log(ux[0]);
+// console.log(ux[1]);
+// console.log(ux[2]);
+// console.log(ux[3]);
+// console.log(ux.length);
+
+
+// var ux = ["user research", "IA", "IA", "UI"];
+// var arrayLength = ux.length;
+// var myList = "<ul>";
+ 
+// for (var i = 0;i <arrayLength; i++) {
+//   myList += "<li>" + ux[i] + "</li>";
+// }
+ 
+// myList += "</ul>";
+ 
+// document.getElementById("minArraySection").innerHTML = myList;
+
+
+// var text="";
+// var i = 0;
+// while (i<=10){
+//     text+="<br>Siffran är "+i;
+//     i++;
+// }
+
+// document.getElementById("minArraySection2").innerHTML = text;
+
+// var myButton = document.getElementById("button2");
+
+// myButton.addEventListener("click", function(){
+//     this.classList.toggle("blue-background");
+// });
+
+var ux = [<"img", "IA", "IA", "UI"];
+ var arrayLength = ux.length;
+    var myList = "<ul>";
+ 
+for (var i = 0;i <arrayLength; i++) {
+     myList += "<li>" + ux[i] + "</li>";
+ }
+ 
+    myList += "</ul>";
+ 
+document.getElementById("minArraySection").innerHTML = myList;
+
+var text="";
+ var i = 0;
+ while (i<=10){
+     text+="<br>Siffran är "+i;
+     i++;
+ }
+
+ document.getElementById("minArraySection2").innerHTML = text;
+
+ var myButton = document.getElementById("button2");
+
+ myButton.addEventListener("click", function(){
+    this.classList.toggle("blue-background");
+    });
+
+
